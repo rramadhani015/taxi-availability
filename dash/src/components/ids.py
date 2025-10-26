85266af13e27b5a7fe6a3d3f66f46388ee2523cc
Rahmat Ramadhani <47129927+rramadhani015@users.noreply.github.com>
2025-10-27 00:02:09 +0700

    Delete dash directory

--- dash/src/components/ids.py
+++
@@ -1,6 +0,0 @@
-# SELECTIONS_DROPDOWN = "selection-dropdown"
-CATEGORY_DROPDOWN = "category-dropdown"
-SELECT_ALL_BUTTON = "select-all-selections"
-SELECT_ALL_CATEGORY = "select-all-category"
-
-BAR_CHART = "bar-chart"
